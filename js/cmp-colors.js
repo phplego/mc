b18d502b6132bf36211f212bb4869f7940add87f
phplego <phplego@gmail.com>
2019-03-10 21:21:55 +0300

    colors hint

--- js/cmp-colors.js
+++ js/cmp-colors.js
@@ -14,7 +14,7 @@ class ColorsComponent
         for(const key in McConst.colors){
             const color = McConst.colors[key];
 
-            result += `<div onclick="alert('${key}')" title="${key}" style="background-color: ${color}; display: inline-block; cursor: pointer; width: 18px; height: 18px; margin-right: 4px"></div>`;
+            result += `<div onclick="alert('${key} = ${color}')" title="${key}" style="background-color: ${color}; display: inline-block; cursor: pointer; width: 18px; height: 18px; margin-right: 4px"></div>`;
         }
         return result;
     }
